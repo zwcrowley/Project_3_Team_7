e044d1a3cef81b7eec66ad5f70624d8ffb896aab
@zwcrowley-qczvvO <zwcrowley@gmail.com>
2023-01-18 21:54:48 -0600

    added files for sampleapp

---
+++ sampleapp/app.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(autoload_with=engine)
+
+# Save reference to the table
+Passenger = Base.classes.test
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/names<br/>"
+        f"/api/v1.0/passengers"
+    )
+
+if __name__ == '__main__':
+    app.run()
