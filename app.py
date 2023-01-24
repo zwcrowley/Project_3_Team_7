7cde518c29b434ffac19105c5682b3ebedffe930
@zwcrowley-qczvvO <zwcrowley@gmail.com>
2023-01-24 12:57:38 -0600

    flask app running locally with mongodb

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import numpy as np
-from flask import Flask, render_template, jsonify
+from flask import Flask, request, render_template, jsonify
 import pymongo
 from pymongo import MongoClient
-from bson import json_util, ObjectId  
 import json
+from bson.json_util import dumps
+
 
 #################################################
 # Flask Setup
@@ -39,29 +40,17 @@ def welcome():
 @app.route("/api/v1.0/home_value_risk_data")
 def home_risk():
     """Return the home risk data as list"""
-    homes = hv_risk_collection.find({})
-
-    all_homes = list(np.ravel([home for home in homes]))
-
-    # print(all_homes) 
-    # return jsonify([i for i in hv_risk_collection.find({})])
-
-    return jsonify([home for home in homes]) 
-
-    #Dump loaded BSON to valid JSON string and reload it as dict
-    # return json.loads(json_util.dumps([home for home in homes]))
-
-    # return json.loads(json_util.dumps([home for home in homes]))
+    homes = hv_risk_collection.find()
+    all_homes = jsonify(dumps(homes))
+    return all_homes  
 
 # US county boundary geojson data route:
 @app.route("/api/v1.0/county_bounds_data")
 def county_lines():
     """Return the county bounds data as list"""
     counties = county_bounds_collection.find()
-
-    return json.loads(json_util.dumps([county for county in counties]))
-
-    # return jsonify([county for county in counties]) 
+    all_counties = jsonify(dumps(counties)) 
+    return all_counties 
 
 if __name__ == "__main__":
     app.run(debug=True)
