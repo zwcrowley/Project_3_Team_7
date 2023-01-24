8caefdcd83b5ac3b068f9433fe3ff003538234f5
@zwcrowley-qczvvO <zwcrowley@gmail.com>
2023-01-24 14:42:53 -0600

    app for real works, got jsons loading for both

--- app.py
+++ app.py
@@ -3,7 +3,20 @@ from flask import Flask, request, render_template, jsonify
 import pymongo
 from pymongo import MongoClient
 import json
-from bson.json_util import dumps
+from collections import defaultdict
+from bson.json_util import dumps, loads 
+import pandas as pd
+from typing import Any
+from bson import ObjectId
+
+# Function to encode mongoDB object_id:
+class MongoJSONEncoder(json.JSONEncoder):
+    def default(self, o: Any) -> Any:
+        if isinstance(o, ObjectId):
+            return str(o)
+        if isinstance(o, datetime):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
 
 
 #################################################
@@ -39,18 +52,25 @@ def welcome():
 # ZHVI data route:
 @app.route("/api/v1.0/home_value_risk_data")
 def home_risk():
-    """Return the home risk data as list"""
+    """Return the home risk data as json"""
     homes = hv_risk_collection.find()
-    all_homes = dumps(homes) 
-    return all_homes  
+    # Convert object_id from the homes mongo cursor:
+    homes_json = MongoJSONEncoder().encode(list(homes))
+    # Convert to python obj:
+    homes_obj = json.loads(homes_json)
+
+    return jsonify(homes_obj)      
 
 # US county boundary geojson data route:
 @app.route("/api/v1.0/county_bounds_data")
 def county_lines():
-    """Return the county bounds data as list"""
+    """Return the county bounds data as json"""
     counties = county_bounds_collection.find()
-    all_counties = dumps(counties)
-    return all_counties 
+    # Convert object_id from the homes mongo cursor:
+    counties_json = MongoJSONEncoder().encode(list(counties))
+    # Convert to python obj:
+    counties_obj = json.loads(counties_json)
+    return jsonify(counties_obj)  
 
 if __name__ == "__main__":
     app.run(debug=True)
