37d6665f291e1be6fe627fa671311279c8fbb618
@zwcrowley-qczvvO <zwcrowley@gmail.com>
2023-01-24 12:36:06 -0600

    Issues with flask app

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import numpy as np
-from flask import Flask, render_template
-import pymongo 
+from flask import Flask, render_template, jsonify
+import pymongo
+from pymongo import MongoClient
+from bson import json_util, ObjectId  
+import json
 
 #################################################
 # Flask Setup
@@ -10,12 +13,13 @@ app = Flask(__name__)
 ###############
 # setup mongo connection
 conn = "mongodb://localhost:27017"
-client = pymongo.MongoClient(conn)
+client = pymongo.MongoClient(conn) 
 
-# connect to mongo db and two collections
+# connect to mongo db: 
 db = client.home_risk_db
-county_bounds_collection = db.county_bounds
+# Connect to two collections:
 hv_risk_collection = db.hv_risk
+county_bounds_collection = db.county_bounds
 
 
 #################################################
@@ -31,5 +35,33 @@ def welcome():
         f"/api/v1.0/county_bounds_data"
     )
 
-if __name__ == '__main__':
-    app.run()
+# ZHVI data route:
+@app.route("/api/v1.0/home_value_risk_data")
+def home_risk():
+    """Return the home risk data as list"""
+    homes = hv_risk_collection.find({})
+
+    all_homes = list(np.ravel([home for home in homes]))
+
+    # print(all_homes) 
+    # return jsonify([i for i in hv_risk_collection.find({})])
+
+    return jsonify([home for home in homes]) 
+
+    #Dump loaded BSON to valid JSON string and reload it as dict
+    # return json.loads(json_util.dumps([home for home in homes]))
+
+    # return json.loads(json_util.dumps([home for home in homes]))
+
+# US county boundary geojson data route:
+@app.route("/api/v1.0/county_bounds_data")
+def county_lines():
+    """Return the county bounds data as list"""
+    counties = county_bounds_collection.find()
+
+    return json.loads(json_util.dumps([county for county in counties]))
+
+    # return jsonify([county for county in counties]) 
+
+if __name__ == "__main__":
+    app.run(debug=True)
