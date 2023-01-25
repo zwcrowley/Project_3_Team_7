24acd552d2ab906e8790adaf6006102a1735b8ad
@zwcrowley-qczvvO <zwcrowley@gmail.com>
2023-01-25 16:42:15 -0600

    Try again

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ class MongoJSONEncoder(json.JSONEncoder):
 #################################################
 app = Flask(__name__)
 # Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://" + mongopass_app
+app.config["MONGO_URI"] = mongopass_app 
 mongo = PyMongo(app)
 # Allow cross origin:
 CORS(app) 
