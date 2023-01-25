7f46a6c5658ea49e7b49cf8a65b358accd28d0b5
@zwcrowley-qczvvO <zwcrowley@gmail.com>
2023-01-25 16:25:28 -0600

    One other way to write pymongo call

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import numpy as np
 from flask import Flask, Response, request, render_template, jsonify
 import pymongo
+from flask_pymongo import PyMongo
 from pymongo import MongoClient
 import json
 from bson.json_util import dumps, loads 
@@ -21,19 +22,22 @@ class MongoJSONEncoder(json.JSONEncoder):
 # Flask Setup
 #################################################
 app = Flask(__name__)
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = mongopass_app
+mongo = PyMongo(app)
 # Allow cross origin:
 CORS(app) 
 
 ###############
 # setup mongo connection
-conn = mongopass_app
-client = pymongo.MongoClient(conn) 
+# conn = mongopass_app
+# client = pymongo.MongoClient(conn) 
 
-# connect to mongo db: 
-db = client.home_risk_db
-# Connect to two collections:
-hv_risk_collection = db.hv_risk
-county_bounds_collection = db.county_bounds
+# # connect to mongo db: 
+# db = client.home_risk_db
+# # Connect to two collections:
+# hv_risk_collection = db.hv_risk
+# county_bounds_collection = db.county_bounds
 
 #################################################
 # Flask Routes
@@ -52,7 +56,7 @@ def welcome():
 @app.route("/api/v1.0/home_value_risk_data")
 def home_risk():
     """Return the home risk data as json"""
-    homes = hv_risk_collection.find()
+    homes = mongo.db.hv_risk.find()
     # Convert object_id from the homes mongo cursor:
     homes_json = MongoJSONEncoder().encode(list(homes))
     # Create response:
@@ -66,7 +70,7 @@ def home_risk():
 @app.route("/api/v1.0/county_bounds_data")
 def county_lines():
     """Return the county bounds data as json"""
-    counties = county_bounds_collection.find()
+    counties = mongo.db.county_bounds.find()
     # Convert object_id from the homes mongo cursor:
     counties_json = MongoJSONEncoder().encode(list(counties))
     # Create response:
