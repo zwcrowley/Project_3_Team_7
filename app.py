d5e9ed3ebe674b2b07dee64a25f6794cfbda8959
@zwcrowley-qczvvO <zwcrowley@gmail.com>
2023-01-27 10:41:49 -0600

    set up all cols needed in app, fixed flood score

--- app.py
+++ app.py
@@ -49,20 +49,30 @@ def home_risk():
 
     """Return a json of the columns below"""
     # Query all columns that we want from the dataset:
-    results = session.query(hv_risk.index, hv_risk.state_county_FIPS, hv_risk.zhvi_yr_growth,hv_risk.risk_index_score).all()
+    results = session.query(hv_risk.index, hv_risk.state_county_FIPS, hv_risk.zhvi_yr_growth, hv_risk.risk_index_score,
+    hv_risk.drought_score, hv_risk.flood_score, hv_risk.heatwave_score, hv_risk.hurricane_score, hv_risk.lightning_score, hv_risk.tornado_scores, hv_risk.wildfire_scores, hv_risk.winterweather_score).all()
     
     session.close()
 
     # Create a dictionary from the row data and append to a list of all_passengers
     all_hv_risk = []
-    for index, state_county_FIPS, zhvi_yr_growth,risk_index_score in results:
+    for index, state_county_FIPS, zhvi_yr_growth, risk_index_score,drought_score, flood_score, heatwave_score, hurricane_score, lightning_score, tornado_scores, wildfire_scores, winterweather_score in results:
         hv_risk_dict = {}
         hv_risk_dict["index"] = index
         hv_risk_dict["state_county_FIPS"] = state_county_FIPS
         hv_risk_dict["zhvi_yr_growth"] = zhvi_yr_growth
         hv_risk_dict["risk_index_score"] = risk_index_score
+        hv_risk_dict["drought_score"] = drought_score
+        hv_risk_dict["flood_score"] = flood_score
+        hv_risk_dict["heatwave_score"] = heatwave_score
+        hv_risk_dict["hurricane_score"] = hurricane_score
+        hv_risk_dict["lightning_score"] = lightning_score
+        hv_risk_dict["tornado_scores"] = tornado_scores
+        hv_risk_dict["wildfire_scores"] = wildfire_scores
+        hv_risk_dict["winterweather_score"] = winterweather_score
+        # Append the hv_risk_dict dictionary to the all_hv_risk list to make a json:
         all_hv_risk.append(hv_risk_dict)
-
+    # Return the all_hv_risk json using jsonify to make it look nice:
     return jsonify(all_hv_risk)  
 
 if __name__ == "__main__":
