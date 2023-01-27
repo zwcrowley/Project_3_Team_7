a9ae273b378efcde189aa98b4ec3cb73e3bfd819
@zwcrowley-qczvvO <zwcrowley@gmail.com>
2023-01-27 14:09:32 -0600

    graph is reactive with mouse click on map

--- static/js/logic.js
+++ static/js/logic.js
@@ -42,30 +42,29 @@ let geojson;
 // d3 call to github for hv_risk data- csv:
 d3.json(hv_risk_render).then(function(hv_risk) {
   console.log("hv_risk", hv_risk)
-  // Function to initialize the first id for the charts and panel with the first Id = 940, passes that name to getData(), the on event in the next function will updata once a new id is selected from the dropdown menu:
-  // function init() {
-  //   let firstArray = [hv_risk[0].flood_score,hv_risk[0].drought_score,hv_risk[0].heatwave_score];
-  //   console.log("firstArray",firstArray)
-  //   makeBarChart(firstArray); 
-  // }
-  // // makeBarChart(hv_risk)
-  // init();
-
 
   // d3 call to github for geo_data data- json:
   d3.json(geoJSON_github).then(function(geo_data) {
     console.log("geo_data", geo_data)
 
-    // Loop through each state data value in the .csv file
+    // Function to initialize the first county hv_risk data, passes that to getData(), the on event in the next function will updata once a new county is selected from the map:
+    function init() {
+      let firstCounty = hv_risk[0];
+      console.log("firstCounty",firstCounty)
+      makeBarChart(firstCounty); 
+      } // end of init()
+      
+    // Nested for loop to save the risk_index_score inside the geojson properties for each feature:
+    // Loop through each state data value in the hv_risk json file:
     for (var i = 0; i < hv_risk.length; i++) {
-      // Grab state_county_FIPS Name
-      var data_FIPS = parseFloat(hv_risk[i].state_county_FIPS); 
-      // Grab data value 
-      var dataValue = parseFloat(hv_risk[i].risk_index_score); 
+      // Grab state_county_FIPS from hv_risk data:
+      let data_FIPS = parseFloat(hv_risk[i].state_county_FIPS); 
+      // Grab risk_index_score and set to var dataValue:
+      let dataValue = parseFloat(hv_risk[i].risk_index_score); 
       // Nested for loop that goes throught the geo_data json:
       // Find the corresponding state_county_FIPS inside the GeoJSON
       for (var j = 0; j < geo_data.features.length; j++) {
-        var json_FIPS = parseFloat(geo_data.features[j].properties.state_county_FIPS); 
+        let json_FIPS = parseFloat(geo_data.features[j].properties.state_county_FIPS); 
         // If the fips matches in both datasets:
         if (data_FIPS === json_FIPS) {
           // Copy the data value into the JSON
@@ -76,12 +75,6 @@ d3.json(hv_risk_render).then(function(hv_risk) {
       }
     }
     
-    // console.log("geo_data.features[j].properties.risk_index_score", geo_data.features[0].properties.risk_index_score)
-    // console.log("dataValue", dataValue, i)
-    function zoomToFeature(e) {
-      myMap.fitBounds(e.target.getBounds());
-    }
-
     // Create a new choropleth layer.
     geojson = L.choropleth(geo_data, {
       // Set the color based on the risk_index_score:
@@ -101,44 +94,15 @@ d3.json(hv_risk_render).then(function(hv_risk) {
       },
         // Binding a popup to each layer
         onEachFeature: function(feature, layer) {
-          layer.bindPopup("County Name: <strong>" + feature.properties.NAME + "</strong><br /><br />Risk Index Score: " +
-          parseFloat(feature.properties.risk_index_score).toFixed(2));
+          layer.bindPopup("County Name: <strong>" + feature.properties.NAME +"<br /><br />"+ feature.properties.state_county_FIPS + "</strong><br /><br />Risk Index Score: " +
+          parseFloat(feature.properties.risk_index_score).toFixed(2)); 
           layer.on({
             click: getData
           }); 
         } 
       }).addTo(myMap); // end of choropleth layer function
 
-      // Function to return data where mouseclick occured:
-      function getData(event) {
-        let county_clicked = event.target.feature.properties.state_county_FIPS;
-        console.log("clicked county", county_clicked)
-        // Reset barGraph
-        // Plotly.purge(graph_1); 
-
-        let barArray = [];
-
-        // Loop through each state data value in the hv_risk json call:
-        for (var i = 0; i < hv_risk.length; i++) {
-          // Grab state_county_FIPS Name
-          let data_FIPS = hv_risk[i].state_county_FIPS;  
-            // If the fips matches the fips from the mouse click event:
-            if (data_FIPS === county_clicked) {
-              barArray.push([hv_risk[i].flood_score,hv_risk[i].drought_score,hv_risk[i].heatwave_score]);
-              console.log("barArray",barArray)
-              console.log("county_clicked",county_clicked)
-              console.log("data_FIPS",data_FIPS)
-              // Stop looking throught the hv_risk data:
-              break;  
-            }
-          }
-        // let featureJson = event.target.feature;
-        // console.log("featureJson", featureJson)
-        // passes barArray_x, and state_county_FIPS to makeBarChart()
-        console.log("barArray",barArray)
-        makeBarChart(barArray);  
-      }; // end of getData()
-    
+      
       // Set up the legend:
       let legend = L.control({ position: "bottomright" });
       legend.onAdd = function() {
@@ -163,139 +127,97 @@ d3.json(hv_risk_render).then(function(hv_risk) {
       };
       // Adding the legend to the map
       legend.addTo(myMap);
-      
-      // Mouse Click:
-      // myMap.on('click', function(e) {
-      //   alert(e.latlng);
-      //   console.log(e);
-      //   // download something
-      //   /* if(something == something){
-      //         e.preventDefault(); 
-      //         window.location.href = 'downloadMe/riverData.doc';
-      //      }
-      //   */
-      // } );
-  
-  }); // end of d3 call to github for hv_risk data- csv:
 
-}); // end of d3 call to github for geo_data- geojson:
+      // Function to return data where mouseclick occured:
+      function getData(event) {
+        let county_clicked = event.target.feature.properties.state_county_FIPS;
+        console.log("clicked county", county_clicked)
+        // Reset barGraph
+        // Plotly.purge(graph_1); 
+
+        // let barArray = [];
+
+        // Next, get data for the bar chart and the bubble chart:
+        
+        // Filter the samples to match the chosen value in the dropdown menu= dataValue, add [0] to the end to pull out that sample key:
+        let hv_risk_Matched = hv_risk.filter(county => county.state_county_FIPS === county_clicked)[0]; 
+        console.log("hv_risk_Matched",hv_risk_Matched)
+        // Pass hv_risk_Matched to all of the charts:
+        // Call "makeBarChart" function to pass the hv_risk_Matched to it: 
+        makeBarChart(hv_risk_Matched); 
+
+        // // Loop through each state data value in the hv_risk json call:
+        // for (var i = 0; i < hv_risk.length; i++) {
+        //   // Grab state_county_FIPS Name
+        //   let data_FIPS = hv_risk[i].state_county_FIPS;  
+        //     // If the fips matches the fips from the mouse click event:
+        //     if (data_FIPS === county_clicked) {
+        //       barArray.push([hv_risk[i].flood_score,hv_risk[i].drought_score,hv_risk[i].heatwave_score]);
+        //       console.log("barArray",barArray)
+        //       console.log("county_clicked",county_clicked)
+        //       console.log("data_FIPS",data_FIPS)
+        //       // Stop looking throught the hv_risk data:
+        //       break;  
+        //     }
+        //   }
+        // let featureJson = event.target.feature;
+        // console.log("featureJson", featureJson)
+        // passes barArray_x, and state_county_FIPS to makeBarChart()
+        // makeBarChart(barArray);  
+      }; // end of getData()
+
+      // Function to make reactive bar chart:
+      function makeBarChart(barArray) {
+        let bar_new = barArray 
+        console.log("bar_new",bar_new)
+        // Set all the vars in the array of to object vars to build the charts, slice the top 10 and reverse them:
+        let barArray_y = ["flood_score","drought_score","heatwave_score"]//, "hurricane_score", "lightning_score", "tornado_scores", "wildfire_scores","winterweather_score"]
+        // let barArray_x = barArray; 
+        let barArray_x = [bar_new.flood_score,bar_new.drought_score,bar_new.heatwave_score].reverse(); 
+
+        console.log("barArray_x", barArray_x)
+        // let bar_risk = barArray.reverse();
+        // let bar_labels = newdata.otu_labels.slice(0, 10).reverse();
+        // let sample_values = newdata.sample_values.slice(0, 10).reverse();
+        // Re-format the otu_ids as labels for the y-axis:
+        // let y_labels = otu_ids.map(otu_id => `OTU ${otu_id}`);
+        // Trace1 for the data of 8 risks:
+        let trace1 = {
+          x: barArray_x,
+          y: barArray_y,
+          // text: otu_ids,
+          // hovertext: otu_labels,
+          name: "Bar1",
+          type: "bar",
+          orientation: "h"
+        };
+        // add the trace1 to a barData array:
+        var barData = [trace1];
+        console.log("barData", barData) 
+
+        // Apply a title to the layout and margins, pull the ID for the title:
+        let layout_bar = {
+          title: `<b>Bar Chart</b>`,
+          margin: {
+            l: 100,
+            r: 100,
+            t: 100,
+            b: 100
+          }
+        };
+        console.log("layout_bar", layout_bar)  
+
+        // Render the plot to the div tag with id "bar", and pass barData and layout:
+        Plotly.newPlot("graph_1", barData, layout_bar);
+      } // end of makeBarChart() call 
+      init();
+  }); // end of d3 call to github for geo_data- geojson.
+
+}); // end of d3 call to render/Flask app for hv_risk data- json.
 
 
-// Function to make reactive bar chart:
-function makeBarChart(barArray) {
-  Plotly.purge(graph_1);
-  // Set all the vars in the array of to object vars to build the charts, slice the top 10 and reverse them:
-  let barArray_y = ["flood_score","drought_score","heatwave_score"]//, "hurricane_score", "lightning_score", "tornado_scores", "wildfire_scores","winterweather_score"]
-  let barArray_x = barArray; 
-  // let barArray_x = [hv_risk[0].flood_score,hv_risk[0].drought_score,hv_risk[0].heatwave_score]; 
 
-  console.log("barArray_x", barArray_x)
-  // let bar_risk = barArray.reverse();
-  // let bar_labels = newdata.otu_labels.slice(0, 10).reverse();
-  // let sample_values = newdata.sample_values.slice(0, 10).reverse();
-  // Re-format the otu_ids as labels for the y-axis:
-  // let y_labels = otu_ids.map(otu_id => `OTU ${otu_id}`);
-  // Trace1 for the data of 8 risks:
-  let trace1 = {
-    x: barArray_x,
-    y: barArray_y,
-    // text: otu_ids,
-    // hovertext: otu_labels,
-    name: "Bar1",
-    type: "bar",
-    orientation: "h"
-  };
-  // add the trace1 to a barData array:
-  var barData = [trace1];
-  console.log("barData", barData) 
 
-  // Apply a title to the layout and margins, pull the ID for the title:
-  let layout_bar = {
-    title: `<b>Bar Chart</b>`,
-    margin: {
-      l: 100,
-      r: 100,
-      t: 100,
-      b: 100
-    }
-  };
-  console.log("layout_bar", layout_bar)  
 
-  // Render the plot to the div tag with id "bar", and pass barData and layout:
-  Plotly.newPlot("graph_1", barData, layout_bar);
-}
-
-// // Note the extra brackets around 'x' and 'y'
-// function updatePlotly(newdata) {
-//   Plotly.restyle("graph_1", "values",[newdata] ) 
-// }
-
-// function getData() {
-//   // Assign the value of the dropdown menu option to a variable:
-//   let dataValue = d3.select("#selDataset").property("value"); 
-//   // First, the metadata for the panel demo table:
-//   // Set metadata to a var:
-//   let metadata = data.metadata;
-//   // Filter the metadata to find the id that was selected in the dropdownMenu (dataValue)):
-//   let chosenMetadata = metadata.filter(meta => meta.id == dataValue);  
-//   let chosenMeta = chosenMetadata[0]  
-
-//   // Call "updatePanel" function to update the panel with the new metadata:
-//   updatePanel(chosenMeta);
-
-//   // Next, get data for the bar chart and the bubble chart:
-//   // Set samples to a var:
-//   let samples = data.samples;
-//   // Filter the samples to match the chosen value in the dropdown menu= dataValue, add [0] to the end to pull out that sample key:
-//   let sampleMatched = samples.filter(s => s.id === dataValue)[0]; 
-
-//   // Pass sampleMatched to all of the charts:
-//   // Call "makeBarChart" function to pass the sampleMatched to it: 
-//   makeBarChart(sampleMatched); 
-//   // Call "makeBubbleChart" function to pass the sampleMatched to it: 
-//   makeBubbleChart(sampleMatched); 
-//   // Call "makeGaugeChart" function to pass the sampleMatched to it: 
-//   makeGaugeChart(chosenMeta); 
-
-// }
-
-// // Function to make the horizontal bar chart for top ten OTU IDS for the chosen subject ID:
-// function makeBarChart(newdata) {
-//   // Set all the vars in the array of sampleMatched (newdata) to object vars to build the charts, slice the top 10 and reverse them:
-//   let otu_ids = newdata.otu_ids.slice(0, 10).reverse();
-//   let otu_labels = newdata.otu_labels.slice(0, 10).reverse();
-//   let sample_values = newdata.sample_values.slice(0, 10).reverse();
-
-//   // Re-format the otu_ids as labels for the y-axis:
-//   let y_labels = otu_ids.map(otu_id => `OTU ${otu_id}`);   
-
-//   // Trace1 for the top 10 belly button data: 
-//   let trace1 = {
-//     x: sample_values,
-//     y: y_labels, 
-//     text: otu_ids,
-//     hovertext: otu_labels,
-//     name: "beboBar",
-//     type: "bar",
-//     orientation: "h"
-//   };
-
-//   // add the trace1 to a barData array:
-//   var barData = [trace1];
-
-//   // Apply a title to the layout and margins, pull the ID for the title:
-//   let layout_bar = {
-//     title: `<b>Top Ten OTUs in ID: ${newdata.id}</b>`, 
-//     margin: {
-//       l: 75,
-//       r: 0,
-//       t: 50,
-//       b: 50
-//     }
-//   };
-
-//   // Render the plot to the div tag with id "bar", and pass barData and layout:
-//   Plotly.newPlot("bar", barData, layout_bar); 
-// }
 
 
